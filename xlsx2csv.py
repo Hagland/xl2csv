3deee93ceed8b92785541d413aba7ae01e63b461
Erik <erik.hagland@gmail.com>
2021-06-09 20:36:06 +0200

    Add Black formatting and requirements.txt

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -8,7 +8,11 @@ import glob
 
 def load_files(pattern, combine):
     files = glob.glob(pattern)
-    files = [(f, openpyxl.load_workbook(f, data_only=True).active) for f in files if f.lower().endswith('.xlsx')]
+    files = [
+        (f, openpyxl.load_workbook(f, data_only=True).active)
+        for f in files
+        if f.lower().endswith(".xlsx")
+    ]
     if not files:
         click.echo(f"No files found for pattern '{pattern}'")
         sys.exit(-1)
@@ -18,14 +22,16 @@ def load_files(pattern, combine):
 def write_separate(files, sep, encoding):
     for f in files:
         name, ws = f
-        with open(name.replace('xlsx', 'csv'), 'w', encoding=encoding) as csv_file:
+        with open(
+            name.replace("xlsx", "csv"), "w", encoding=encoding
+        ) as csv_file:
             writer = csv.writer(csv_file, delimiter=sep, quoting=csv.QUOTE_ALL)
             for row in ws.rows:
                 writer.writerow([cell.value for cell in row])
 
 
 def write_combined(files, output, sep, encoding):
-    with open(output, 'w', encoding=encoding) as csv_file:
+    with open(output, "w", encoding=encoding) as csv_file:
         writer = csv.writer(csv_file, delimiter=sep, quoting=csv.QUOTE_ALL)
         for i, f in enumerate(files):
             name, ws = f
@@ -35,11 +41,32 @@ def write_combined(files, output, sep, encoding):
 
 
 @click.command()
-@click.option('-p', '--pattern', default='*.xlsx', help='Pattern to match input files, default *.xlsx')
-@click.option('-d', '--delimiter', default=',', help='Default csv delimiter')
-@click.option('-e', '--encoding', default='cp1252', help="The input file's encoding, default cp1252")
-@click.option('-c', '--combined', default=False, is_flag=True, help='Combine all inputs into a single output file')
-@click.option('-o', '--output', default='combined.csv', help='Output file for combined files')
+@click.option(
+    "-p",
+    "--pattern",
+    default="*.xlsx",
+    help="Pattern to match input files, default *.xlsx",
+)
+@click.option("-d", "--delimiter", default=",", help="Default csv delimiter")
+@click.option(
+    "-e",
+    "--encoding",
+    default="cp1252",
+    help="The input file's encoding, default cp1252",
+)
+@click.option(
+    "-c",
+    "--combined",
+    default=False,
+    is_flag=True,
+    help="Combine all inputs into a single output file",
+)
+@click.option(
+    "-o",
+    "--output",
+    default="combined.csv",
+    help="Output file for combined files",
+)
 def main(pattern, delimiter, encoding, combined, output):
     files = load_files(pattern, combined)
     if combined:
@@ -47,5 +74,6 @@ def main(pattern, delimiter, encoding, combined, output):
     else:
         write_separate(files, delimiter, encoding)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
